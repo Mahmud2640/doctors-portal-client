1d714b965fdad641825e6bbf2331acd6d2523c0d
Mahmud2640 <mdmahmud484@gmail.com>
2022-05-11 06:23:48 +0600

    treatment

--- src/Components/Page/Home/Home.js
+++ src/Components/Page/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Experiment from './Experiment/Experiment';
 import Info from './Info/Info';
+import Services from './Service/Services';
 
 const Home = () => {
    return (
-      <div>
+      <div className="px-12">
          <Banner></Banner>
          <Info></Info>
+         <Services></Services>
+         <Experiment></Experiment>
       </div>
    );
 };
--- src/Components/Page/Home/Info/Info.js
+++ src/Components/Page/Home/Info/Info.js
@@ -6,20 +6,20 @@ import InfoCard from "./InfoCard/InfoCard";
 
 const Info = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 px-12">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
       <InfoCard
         cardTitle="Opening Hours"
-        bgClass="bg-gradient-to-r from-secondary to primary"
+        bg="bg-primary"
         img={clock}
       ></InfoCard>
       <InfoCard
         cardTitle="Visit Our Location"
-        bgClass="bg-accent"
+        bg="bg-accent"
         img={marker}
       ></InfoCard>
       <InfoCard
         cardTitle="Contact Us Now"
-        bgClass="bg-gradient-to-r from-secondary to primary"
+        bg="bg-secondary"
         img={phone}
       ></InfoCard>
     </div>
